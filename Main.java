908cfe385f308084775e48c74cf42808504ff980
nishimototo <dkspm2436@gmail.com>
2021-10-31 14:08:20 +0900

    9-2完了

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
 public class Main {
+  public static void heal(int hp) {
+    hp += 10;
+}
+
+  public static void heal(Theif theif) {
+    theif.hp += 10;
+}
+
   public static void main(String[] args) {
-    Hero h = new Hero("湊");
-    Hero h2 = new Hero();
-    Theif t1 = new Theif("アサカ", 50, 10);
-    
-    System.out.println(h.hp);
-    System.out.println(h.name);
-    System.out.println("名前は" + h2.name);
-    System.out.println(t1.name + "HP:" + t1.hp + " MP:" + t1.mp);
-  }  
+    int baseHp = 25;
+    Theif t = new Theif("アサカ", baseHp);
+    System.out.println(baseHp + ":" + t.hp); //t.hpがbaseHpと同じ25
+    heal(baseHp);//hpが35になる
+    heal(t);
+    System.out.println(baseHp + ":" + t.hp)
+}
 }
