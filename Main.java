b78104589f3be7ec4d3fa69ca3b6509650a8a795
nishimototo <dkspm2436@gmail.com>
2021-10-31 10:41:03 +0900

    8章学習

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
-public class Main {
-  public static void main(String[] args) {
-    int a = 3;
-    System.out.println(a);
-  }
+public class Main {public static void main(String[] args) {
+  Hero h = new Hero();
+  h.name = "ミナト";
+  h.hp = 100;
+  System.out.println("勇者" + h.name + "を生み出しました");
+  h.sit(5);
+  h.slip();
+  h.run();
+}
 }
