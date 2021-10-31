d47164c48dde25cb986a8c0f60afc898bcc003a4
nishimototo <dkspm2436@gmail.com>
2021-10-31 11:16:50 +0900

    8章の演習完了

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
-public class Main {public static void main(String[] args) {
-  Hero h = new Hero();
-  h.name = "ミナト";
-  h.hp = 100;
-  System.out.println("勇者" + h.name + "を生み出しました");
-  h.sit(5);
-  h.slip();
-  h.run();
-}
+public class Main {
+  public static void main(String[] args) {
+    Hero h = new Hero();
+    h.name = "ミナト";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました");
+    
+    Cleric c = new Cleric();
+    c.name = "田中";
+    c.selfAid();
+    c.pray(2);
+  }
 }
