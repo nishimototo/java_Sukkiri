2d1fc73c040680e82ed31e1f3b5160891623ab83
nishimototo <dkspm2436@gmail.com>
2021-11-03 22:21:56 +0900

    13章演習

---
+++ Wand.java
@@ -0,0 +1,20 @@
+public class Wand {
+  private String name;
+  private double power;
+  
+  public String getName() {
+      return this.name;
+  }
+  
+  public setName(String name) {
+      this.name = name;
+  }
+  
+  public double getPower() {
+      return this.power;
+  }
+  
+  public setPower(double power) {
+      this.power = power;
+  }
+}
