b4d88cdf2e41720cbb1fcba4b8749f8ac22ec281
nishimototo <dkspm2436@gmail.com>
2021-11-04 09:59:52 +0900

    2章の演習 復習

--- Practice2.java
+++ Practice2.java
@@ -11,5 +11,5 @@ public class Practice2 {
     System.out.println("占いの結果が出ました");
     System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
     System.out.println("1:大吉 2:中吉 3:吉 4:凶");
-  }
+  } 
 }
