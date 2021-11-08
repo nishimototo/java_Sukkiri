5969fd80429384ba67b35ef192dad3f760767082
nishimototo <dkspm2436@gmail.com>
2021-11-08 11:28:08 +0900

    10章復習

--- PoisonMatango.java
+++ PoisonMatango.java
@@ -1,16 +1,17 @@
 public class PoisonMatango {
-  int poisonCount = 5;
-    public PoisonMatango(char suffix) {
+    int poisonCount = 5;
+    public poisonMatango(char suffix) {
         super(suffix);
     }
+    
     public void attack(Hero h) {
-        super.attack(h)
+        super.attack(h);
         if (this.poisonCount > 0) {
-             System.out.println("さらに毒の胞子をばらまいた");
-             int dmg = h.hp / 5;
-             h.hp -= dmg;
-             System.out.println(dmg + "ポイントのダメージ");
-             this.poisonCount--;
-        }
+            System.out.println("さらに毒の胞子をばらまいた");
+            int damage = h.hp / 5;
+            h.hp -= damage;
+            System.out.println(damage + "のダメージ");
+            this.poisonCount--;
+        } 
     }
 }
