5b9e0a3119ac3ce6079e3bfbdc43dc757f270f83
nishimototo <dkspm2436@gmail.com>
2021-11-08 10:40:36 +0900

    8章の演習 復習

--- Cleric.java
+++ Cleric.java
@@ -1,23 +1,23 @@
 public class Cleric {
-  　String name;
+    String name;
     int hp = 50;
-    final int MAX_HP = 50;
+    Final int MAX_HP = 50;
     int mp = 10;
-    final int MAX_MP = 10;
+    Final int MAX_MP = 10;
     
     public void selfAid() {
         System.out.println(this.name + "はセルフエイドを唱えた");
         this.hp = this.MAX_HP;
         this.mp -= 5;
-        System.out.println(this.name + "のHPが最大まで回復した");
+        System.out.println("HPが最大まで回復した");
     }
     
-    public int pray(int sec) {
-        int recover = sec + new java.util.Random().nextInt(3);
-        System.out.println(this.name + "は" + sec + "秒、祈りを捧げた");
+    public int pry(int sec) {
+        System.out.println(this.name + "は" + sec + "秒間、祈りを捧げた");
+        int recover = sec + new java.util.Random.nextInt(3);
         int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
         this.mp += recoverActual;
-        System.out.println("MP" + "が" + recoverActual + "回復した");
+        System.out.println("MPが" + recoverActual + "回復した");
         return recoverActual;
     }
 }
