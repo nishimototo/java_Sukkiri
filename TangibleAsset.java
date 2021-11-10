5339974529a491fe5d7fc5fcd5bf16dde0b8482b
nishimototo <dkspm2436@gmail.com>
2021-11-10 22:54:58 +0900

    11章演習2

--- TangibleAsset.java
+++ TangibleAsset.java
@@ -1,14 +1,9 @@
-public abstract class TangibleAsset {
-  String name;
-  int price;
+public abstract class TangibleAsset extends Asset {
   String color;
   
   public TangibleAsset (String name, int price, String color) {
-      this.name = name;
-      this.price = price;
-      this.color - color;
+      super(name, price);
+      this.color = color;
   }
-  public String getName() { return this.name; }
-  public String getPrice() { return this.price; }    
   public String getColor() { return this.color; }
 }
