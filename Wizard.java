e2caad24ba43efaaa665926d1317ab75f0dd3ce6
nishimototo <dkspm2436@gmail.com>
2021-11-13 10:30:33 +0900

    13章演習3

--- Wizard.java
+++ Wizard.java
@@ -6,8 +6,18 @@ public class Wizard {
   
   public void heal(Hero h) {
       int basePoint = 10;
-      int recovPoint = (int) (basePoint * this.wand.point);
+      int recovPoint = (int) (basePoint * this.getWand().getPower(););
       h.setHp(h.getHp() + recovPoint);
       System.out.println(h.getName() + "のHPを" + recovPoint + "回復した");
   }
+
+  public int getHp() {return this.hp;}
+  public void SetHp(int hp) {this.hp = hp;}
+  public int getMp() {return this.mp;}
+  public void SetMp(int mp) {this.mp = mp;}
+  public String getName(() {return this.name;}
+  public void SetName(String name) {this.name = name;}
+  public Wand getWand(() {return this.wand;}
+  public void SetWand(Wand wand) {this.wand = wand;}
+}
 }
