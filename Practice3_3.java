a85a0677f2dd15742dfdd03fe53d3ed9f21cf380
nishimototo <dkspm2436@gmail.com>
2021-11-26 22:47:31 +0900

    3章復習

--- Practice3_3.java
+++ Practice3_3.java
@@ -1,15 +1,15 @@
 
 public class Practice3_3 {
   public static void main(String[] args) {
-    int isHungry = new java.util.Random().nextInt();
-    String food = "うどん";
+    int isHungry = new java.util.Random().nextInt(2);
+    String food = "肉";
     System.out.println("こんにちは");
     if (isHungry == 0) {
-      System.out.println("お腹いっぱいです");
+        System.out.println("お腹がいっぱいです");
     } else {
-      System.out.println("腹ペコです");
-      System.out.println(food + "をいただきます");
+        System.out.println("腹ぺこです");
+        System.out.println(food + "をいただきます");
     }
-    System.out.println("ごちそうさまでした");
+    System.out.println("ご馳走様です");
   }
 }
--- Practice3_5.java
+++ Practice3_5.java
@@ -1,20 +1,23 @@
 public class Practice3_5 {
     public static void main(String[] args) {
-        System.out.print("[メニュー]1:検索2:登録3:削除4:変更>");
+        System.out.print("[メニュー]1:検索 2:削除 3:削除 4:変更>");
         int selected = new java.util.Scanner(System.in).nextInt();
         switch (selected) {
-        case 1:
-            System.out.println("検索します");
-            break;
-        case 2:
-            System.out.println("登録します");
-            break;
-        case 3:
-            System.out.println("削除します");
-            break;
-        case 4:
-            System.out.println("変更します");
-            break;
+             case 1:
+                System.out.println("検索します");
+                break;
+             case 2:
+                System.out.println("登録します");
+                break;
+             case 3:
+                System.out.println("削除します");
+                break;
+             case 4:
+                System.out.println("変更します");
+                break;   
+             default:
+                System.out.println("あ、説明効かないタイプの人だ");
+                break;
         }
     }
 }
