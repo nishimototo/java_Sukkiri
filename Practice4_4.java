f4ca0117687eeaa01d5edbfb761bbb34c2fcea9c
nishimototo <dkspm2436@gmail.com>
2021-11-27 21:55:59 +0900

    4章復習

--- Practice4_4.java
+++ Practice4_4.java
@@ -1,13 +1,15 @@
 public class Practice4_4 {
-   int[] numbers = {3, 4, 9};
-   System.out.println("1桁の数字を入力してください");
-   int input = new java.util.Scanner(System.in).nextInt();
-   for(int i = 0; i < numbers.length; i++) {
-      if (numbers[i] == input) {
-         System.out.println("当たり！");
-         break;
-      } else {
-         System.out.println("ハズレ！");
+   public static void main(String[] args) {
+      int[] numbers = { 3, 4, 9 };
+      System.out.println("1桁の数字を入力してください");
+      int input = new java.util.Scanner(System.in).nextInt();
+      for (int n : numbers) {
+         if (n == input) {
+            System.out.println("当たり！");
+            break;
+         } else {
+            System.out.println("ハズレ！");
+         }
       }
    }
 }
