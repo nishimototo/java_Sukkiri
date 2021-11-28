b2a41ff527d3e60891b4e27bb14e4ad033b6b2e1
nishimototo <dkspm2436@gmail.com>
2021-11-28 15:29:34 +0900

    5章復習

--- Practice5_4.java
+++ Practice5_4.java
@@ -1,16 +1,15 @@
 public class Practice5_4 {
   public static void main(String[] args) {
-    double triangleArea = calcTriangleArea(10.0, 5.0);
-    double circleArea = calcCircleArea(5.0);
-    System.out.println("三角形の面積は" + triangleArea + "です");
-    System.out.println("円の面積は" + circleArea + "です");
+    System.out.println("底辺が10.0ｃｍ、高さが5.0ｃｍの三角形の面積は" + calcTriangleArea(10.0, 5.0));
+    System.out.println("半径が5.0cmの円の面積は" + calcCircleArea(5.0));
+
   }
 
   public static double calcTriangleArea(double bottom, double height) {
-      return bottom * height / 2;
+    return bottom * height / 2;
   }
 
   public static double calcCircleArea(double radius) {
-      return radius * radius * 3.14;
+    return radius * radius * 3.14;
   }
 }
